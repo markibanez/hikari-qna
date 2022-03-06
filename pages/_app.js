6e0cd39a946770974439d74464b7ad2cbc14929d
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-06 09:52:27 +0800

    Reworked layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import '../styles/globals.css';
+
+const theme = createTheme({
+    shape: {
+        borderRadius: 15
+    },
+    palette: {
+        background: {
+            default: '#63666A',
+            paper: '#63666A',
+        },
+        text: {
+            primary: '#E6EEF7'
+        },
+        primary: {
+            main: '#E6EEF7'
+        },
+        secondary: {
+            main: '#FFF'
+        },
+        action: {
+            active: '#FFF'
+        },
+
+    }
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-import { Box, Button, Card, CardContent, Grid, IconButton, Stack, TextField, Typography } from '@mui/material';
+import { Box, Button, Card, CardContent, Fab, Grid, IconButton, Stack, TextField, Typography } from '@mui/material';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import shortid from 'shortid';
 import styles from '../styles/Home.module.css';
 import ArrowCircleUpTwoToneIcon from '@mui/icons-material/ArrowCircleUpTwoTone';
+import AddCommentTwoToneIcon from '@mui/icons-material/AddCommentTwoTone';
 
 const data = [
     {
@@ -20,11 +22,22 @@ const data = [
         question: 'What are our plans for remote work?',
         votes: 21,
     },
+    {
+        name: 'Caesar',
+        question:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum tristique dictum. Aenean consectetur eu nisi nec convallis. Vivamus auctor dui iaculis eros fringilla, eu finibus felis ultricies.',
+        votes: 21,
+    },
 ];
 
 export default function Home() {
     const [name, setName] = useState('');
     const [question, setQuestion] = useState('');
+
+    useEffect(() => {
+        const uid = localStorage.getItem('uid');
+        if (!uid) localStorage.setItem('uid', shortid.generate());
+    }, []);
 
     const submitQuestion = async () => {
         alert(`Hello ${name}, you asked the following question: "${question}"`);
@@ -39,10 +52,10 @@ export default function Home() {
             </Head>
 
             <main>
-                <Box
+                {/* <Box
                     sx={{
                         width: '100%',
-                        backgroundColor: 'white',
+                        backgroundColor: 'transparent',
                         position: 'fixed',
                         top: 0,
                         left: 0,
@@ -74,37 +87,38 @@ export default function Home() {
                             </Stack>
                         </CardContent>
                     </Card>
-                </Box>
-                <Stack
-                    direction="column"
-                    spacing={2}
-                    sx={{ position: 'absolute', top: 296, width: '100', left: 0, right: 0, paddingX: '10px' }}
-                >
+                </Box> */}
+                <Grid container spacing={2} alignItems="stretch" sx={{ paddingY: 4 }}>
                     {data.map((q, index) => {
                         return (
-                            <Card variant="outlined" key={index} sx={{ width: '100%' }}>
-                                <CardContent>
-                                    <Grid container spacing={2}>
-                                        <Grid item xs={3}>
-                                            <Stack direction="column" alignItems="center">
-                                                <IconButton>
-                                                    <ArrowCircleUpTwoToneIcon fontSize="large" />
-                                                </IconButton>
-                                                <Typography variant="body1">{q.votes} Votes</Typography>
-                                            </Stack>
+                            <Grid item xs={12} sm={12} md={4} lg={3}>
+                                <Card variant="outlined" key={index} sx={{ width: '100%', height: '100%' }}>
+                                    <CardContent>
+                                        <Grid container spacing={2}>
+                                            <Grid item xs={3}>
+                                                <Stack direction="column" alignItems="center">
+                                                    <IconButton>
+                                                        <ArrowCircleUpTwoToneIcon fontSize="large" />
+                                                    </IconButton>
+                                                    <Typography variant="body1">{q.votes} Votes</Typography>
+                                                </Stack>
+                                            </Grid>
+                                            <Grid item xs={9}>
+                                                <Stack direction="column" spacing={2}>
+                                                    <Typography variant="h6">{q.name}</Typography>
+                                                    <Typography variant="body1">{q.question}</Typography>
+                                                </Stack>
+                                            </Grid>
                                         </Grid>
-                                        <Grid item xs={9}>
-                                            <Stack direction="column" spacing={2}>
-                                                <Typography variant="h6">{q.name}</Typography>
-                                                <Typography variant="body1">{q.question}</Typography>
-                                            </Stack>
-                                        </Grid>
-                                    </Grid>
-                                </CardContent>
-                            </Card>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
                         );
                     })}
-                </Stack>
+                </Grid>
+                <Fab variant="extended" color="primary" sx={{ position: 'fixed', bottom: 40, right: 40 }}>
+                    <AddCommentTwoToneIcon sx={{ marginRight: 1 }} /> Submit Question
+                </Fab>
             </main>
         </div>
     );
