7159531f3cb296a5f50374d25c78746c2c7637ae
Mark Ibanez <mark.ibanez@gmail.com>
2022-03-06 20:41:20 +0800

    Added limit of 50 questions

--- pages/api/get-questions.js
+++ pages/api/get-questions.js
@@ -2,7 +2,7 @@ import { getDB } from './_getDB';
 
 const handler = async (req, res) => {
     const db = await getDB();
-    const data = await db.collection('questions').find().sort({ votes: 1 }).toArray();
+    const data = await db.collection('questions').find().limit(50).sort({ votes: 1 }).toArray();
     res.status(200).json(data);
 }
 
